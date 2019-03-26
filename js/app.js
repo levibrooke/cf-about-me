a8ca18679afeff882e18e6df5eb851712e833f17
Levi Porter <levi@levibrooke.com>
2019-03-26 15:17:54 -0700

    Add button to index, add questions and prompt function to app

---
+++ js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/*
+Questions for guessing game
+- Is my name Levi?
+- Did I live in Hawaii before Washington?
+- Am I the Javascript God?
+- Do I like to play soccer?
+- Is my favorite team the Sounders?
+*/
+
+let questions = ["Is my name Levi?", "Did I live in Hawaii?", "Am I the Javascript God?", "Do I like to play soccer?", "Is my favorite team the Sounders?"];
+
+let responses = [];
+
+let askQuestion = (arr) => {
+  for (var i = 0; i < arr.length; i++) {
+    let answer = prompt(arr[i]);
+
+    if (answer.toLowerCase() === 'no') {
+      responses.push(answer.toLowerCase());
+    } else if (answer.toLowerCase() === 'yes') {
+      responses.push(answer.toLowerCase());
+    } else {
+      alert('You didn\'t answer yes or no');
+      console.log(arr[i]);
+      let nullQuestion = [arr[i]];
+      askQuestion(nullQuestion);
+    }
+    console.log(arr[i], answer);
+  }
+};
+
+document.addEventListener('click', () => {
+  askQuestion(questions);
+});
